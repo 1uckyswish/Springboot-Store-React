4d583e11cee145cfbc321b93d179c65c61bd2a57
Noel Guillen <noelblas274@gmail.com>
2024-05-28 14:04:52 -0400

    added default url

--- recipe/src/main/java/com/noel/recipe/RecipeApplication.java
+++ recipe/src/main/java/com/noel/recipe/RecipeApplication.java
@@ -2,12 +2,19 @@ package com.noel.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RecipeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to the Recipe Application!";
+	}
 }
